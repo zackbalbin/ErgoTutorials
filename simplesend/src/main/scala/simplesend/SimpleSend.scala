7d244e347c5080a6aa79f1fa3b016fc3e36230b6
Zack Balbin <zack@zackbalbin.com>
2021-12-03 17:55:22 -0800

    Create comments for SimpleSend

--- simplesend/src/main/scala/simplesend/SimpleSend.scala
+++ simplesend/src/main/scala/simplesend/SimpleSend.scala
@@ -1,19 +1,25 @@
 package simplesend
 
+// Appkit Libraries
 import org.ergoplatform.appkit._
 import org.ergoplatform.appkit.config.{ErgoNodeConfig, ErgoToolConfig}
 
 object SimpleSend {
 
+  // Function to send transaction
   def sendTx(configFileName: String): String = {
+    // Configuration Setup
     val config: ErgoToolConfig = ErgoToolConfig.load(configFileName)
     val nodeConfig: ErgoNodeConfig = config.getNode()
     val ergoClient: ErgoClient = RestApiErgoClient.create(nodeConfig)
 
+    // Variable imports from config file
     val addressIndex: Int = config.getParameters().get("addressIndex").toInt
     val recieverWalletAddress: Address = Address.create(config.getParameters().get("recieverWalletAddress"))
 
+    // All the logic to create and send the transaction
     val txJson: String = ergoClient.execute((ctx: BlockchainContext) => {
+      // Create a prover object with data from nodeConfig
       val prover: ErgoProver = ctx.newProverBuilder()
         .withMnemonic(
           SecretString.create(nodeConfig.getWallet().getMnemonic()),
@@ -21,6 +27,7 @@ object SimpleSend {
         .withEip3Secret(addressIndex)
         .build()
 
+      // Get funds info from wallet
       val wallet: ErgoWallet = ctx.getWallet()
       val amountToSpend: Long = Parameters.OneErg
       val totalToSpend: Long = amountToSpend + Parameters.MinFee
@@ -30,6 +37,7 @@ object SimpleSend {
 
       val txBuilder = ctx.newTxBuilder()
       
+      // Create new UTXO to spend
       val newBox = txBuilder.outBoxBuilder()
         .value(amountToSpend)
         .contract(ctx.compileContract(
@@ -40,6 +48,7 @@ object SimpleSend {
         ) 
         .build()
 
+      // Create new transaction from the created UTXO
       val tx: UnsignedTransaction = txBuilder
         .boxesToSpend(boxes.get)
         .outputs(newBox)
@@ -47,17 +56,23 @@ object SimpleSend {
         .sendChangeTo(prover.getP2PKAddress())
         .build()
 
+      // Sign the new transaction
       val signed: SignedTransaction = prover.sign(tx)
 
+      // Send the new transaction
       val txId: String = ctx.sendTransaction(signed)
 
+      // Convert the transaction information to JSON
       signed.toJson(true)
     })
+    // Return transaction JSON
     txJson
   }
 
   def main(args: Array[String]): Unit = {
+    // Call sendTx function with the config file name as the parameter
     val txJson: String = sendTx("ergo_config.json")
+    // Print out JSON information
     System.out.println(txJson)
   }
 }
