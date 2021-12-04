1d4826fc69aa1bfe29f14053dea7ca4eb30aa3b4
Zack Balbin <zack@zackbalbin.com>
2021-12-03 19:52:37 -0800

    Create MintToken tutorial

---
+++ minttoken/build.sbt
@@ -0,0 +1,5 @@
+name := "minttoken"
+
+scalaVersion := "2.12.15"
+
+libraryDependencies += "org.ergoplatform" %% "ergo-appkit" % "4.0.5"
---
+++ minttoken/src/main/scala/example/MintToken.scala
@@ -0,0 +1,11 @@
+package minttoken
+
+import org.ergoplatform.appkit._
+import org.ergoplatform.appkit.config.{ErgoNodeConfig, ErgoToolConfig}
+
+object MintToken {
+
+  def main(args: Array[String]): Unit = {
+    println("Mint token tutorial")
+  }
+}
